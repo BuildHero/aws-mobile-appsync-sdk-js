0c8953609a3174827a38bb8ab5628bfe24489d54
Rameez Khan <rameez@wednesday.is>
2020-06-01 22:41:27 +0530

    add fetch to retrieve file blob from path

--- packages/aws-appsync/src/link/complex-object-link-uploader.native.ts
+++ packages/aws-appsync/src/link/complex-object-link-uploader.native.ts
@@ -1,27 +1,33 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 /*!
  * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import * as S3 from 'aws-sdk/clients/s3';
-
-export default (fileField, { credentials }) => {
-    const {
-        bucket: Bucket,
-        key: Key,
-        region,
-        mimeType: ContentType,
-        localUri: Body,
-    } = fileField;
-
-    const s3 = new S3({
-        credentials,
-        region,
-    });
-
-    return s3.upload({
-        Bucket,
-        Key,
-        Body,
-        ContentType,
-    }).promise();
+var S3 = require('aws-sdk/clients/s3');
+exports.default = function (fileField, _a) {
+	var credentials = _a.credentials;
+	var Bucket = fileField.bucket,
+		Key = fileField.key,
+		region = fileField.region,
+		ContentType = fileField.mimeType,
+		Body = fileField.localUri;
+	var s3 = new S3({
+		credentials: credentials,
+		region: region,
+	});
+	return new Promise((resolve, reject) => {
+		fetch(fileField.localUri).then((data) => {
+			resolve(
+				s3
+					.upload({
+						Bucket: Bucket,
+						Key: Key,
+						Body: data._bodyBlob,
+						ContentType: ContentType,
+					})
+					.promise()
+			);
+		});
+	});
 };
